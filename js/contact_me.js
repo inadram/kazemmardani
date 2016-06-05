7f14c813626cd38937e9cad35f6354812182c4ca
inadram <inadram@gmail.com>
2016-06-05 22:24:16 +0100

    update email address

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://formspree.io/inadram@gmail.com",
+                url: "https://formspree.io/info@kazemmardani.com",
                 type: "POST",
 	            headers: {
 		            'Accept': 'application/json',
